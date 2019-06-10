2aeb4dc9256d4772004f34adaec27f47d8682f30
Kevin Evans <kevin.h.evans@wsu.edu>
2019-06-09 17:23:54 -0700

    cleanup

--- usartbootloader.cpp
+++ usartbootloader.cpp
@@ -1,5 +1,102 @@
 #include "usartbootloader.h"
 
+void USARTBootloader::sendCallback(const BootloaderCompletionStatus status, const char* message) {
+    if (this->cbComplete) {
+        BootloaderCompletionState state = {
+            status,
+            message
+        };
+
+        this->cbComplete(state);
+    }
+
+    // TODO: call destroy
+}
+
+
+/**
+ * Initializes the command handlers by mapping commands to function ptrs.
+ */
+void USARTBootloader::initializeCommandHandlers() {
+    commandMap[Get] = 0x00; // the value is ALWAYS 0x00 for the Get command
+
+    commandHandlers[Get] = &USARTBootloader::handleGet;
+    commandHandlers[GetVersionAndRPS] = &USARTBootloader::handleGetVersionAndRPS;
+    commandHandlers[GetID] = &USARTBootloader::handleGetID;
+    commandHandlers[ReadMemory] = &USARTBootloader::handleReadMemory;
+    commandHandlers[Go] = &USARTBootloader::handleGo;
+    commandHandlers[WriteMemory] = &USARTBootloader::handleWriteMemory;
+    commandHandlers[EraseMemory] = &USARTBootloader::handleEraseMemory;
+    commandHandlers[WriteProtect] = &USARTBootloader::handleWriteProtect;
+    commandHandlers[ReadoutProtect] = &USARTBootloader::handleReadoutProtect;
+    commandHandlers[ReadoutUnprotect] = &USARTBootloader::handleReadoutUnprotect;
+}
+
+
+/**
+ * Blows things up
+ */
+void USARTBootloader::destroy() {
+    this->queue.break_dispatch();
+    this->eventThread.terminate();
+
+    int closeStatus = this->usart.close();
+
+    if (closeStatus) {
+        logger.write("failed to close usart, status=%d", closeStatus);
+    }
+
+    sendCallback(StatusUpdate, "USARTBootloader instance destroyed");
+}
+
+/**
+ * Handler for any type of IO signal.
+ * 
+ * NOTE: within an ISR!
+ */ 
+void USARTBootloader::handleIOEvent() {
+    // DO NOT print from this function. It's within an ISR and it will seriously mess things up.
+    // mbed will allow you to do pc.printf() but do NOT do that.
+    short current_events = this->usart.poll(0);
+    
+    if (current_events & POLLIN) {
+        // https://github.com/ARMmbed/mbed-os/blob/master/drivers/UARTSerial.cpp#L126
+        queue.call(callback(this, &USARTBootloader::handleReadRequested));
+    }
+}
+
+/**
+ * Handler for an event in the queue.
+ */
+void USARTBootloader::handleReadRequested() {
+    size_t size;
+    uint8_t cmd = usart_getc(size);
+    
+    switch(cmd) {
+        case ACK:
+            logger.write(">> START ACK >>>>>>>>>>>>>>");
+            handleACK();
+            logger.write("<< DONE ACK <<<<<<<<<<<<<<<");
+            break;
+
+        case NACK:
+            logger.write("NACK. TODO: handle this elegantly");
+            break;
+
+        default:
+            logger.write("<!> invalid command: %x", cmd);
+            
+            if (!queue.has_next() && (this->usart.poll(0) & 1)) {
+                logger.write("\t-> there seems to be more in the rxfifo and an empty equeue");
+                logger.write("\t\t-> will re-enqueue another check");
+
+                queue.call(callback(this, &USARTBootloader::handleReadRequested));
+            }
+            break;
+    }
+}
+
+
 void USARTBootloader::handleGet() {
     logger.write("\treceived Get!");
     size_t r;
@@ -77,10 +174,7 @@ void USARTBootloader::handleReadoutProtect() {
 
 void USARTBootloader::handleReadoutUnprotect() {
     logger.write("\teceived first ack for 0x92, waiting for ack after reset...");
-    
-    size_t s;
-    uint8_t ack2 = this->usart_getc(s);
-    usart.enable_input(false);
+    uint8_t ack2 = this->usart_getc(); // this should be OK if it's blocking, right?
 
     if (ack2 == ACK) {
         logger.write("\trecv'd second ack!");
@@ -96,3 +190,4 @@ void USARTBootloader::handleReadoutUnprotect() {
         sendCallback(TargetNotFlashed, "missing second ACK from 0x92");
     }
 }
+
--- usartbootloader.h
+++ usartbootloader.h
@@ -1,19 +1,33 @@
 #ifndef USARTBOOTLOADER_H
 #define USARTBOOTLOADER_H
 
-#define USART_BAUDRATE 19200
-#define DEVICE_RESET_WAIT_MS 200
+#define DEVICE_USART_BAUDRATE 19200
+
+/**
+ * It should be at least 500ms.
+ * Not sure if there is a duration in the documentation, but from testing,
+ * I've found it takes around 500ms on average.
+ */ 
+#define DEVICE_RESET_WAIT_MS 500
+
+/**
+ * The bootloader should only support 11 commands, but I think for older
+ * bootloader versions, it may only support less.
+ */  
 #define DEVICE_BOOTLOADER_COMMANDS_LENGTH 11
+
 #include "mbed.h"
 #include "EventQueue2.h"
-//#include <map>
 #include "Log2.h"
 
+/**
+ * I should come up with a better name for these enums. 
+ */ 
 typedef enum BootloaderCommand_t {
-    NoLastCommand = -1,
-    MagicKey = 0x7F,
-    ACK = 0x79,
-    NACK = 0x1F
+    NoLastCommand = 0xFF,
+    MagicKey      = 0x7F,
+    ACK           = 0x79,
+    NACK          = 0x1F
 } BootloaderCommand;
 
 typedef enum BootloaderCommandIndex_t {
@@ -59,7 +73,7 @@ class USARTBootloader {
 private:
     Log2 logger;
 
-    int lastCommandSent;
+    uint8_t lastCommandSent;
     Thread eventThread;
     EventQueue2 queue;
     mbed::DigitalOut boot0;
@@ -67,43 +81,34 @@ private:
     mbed::UARTSerial usart;
     
     const char* targetFilename;
+
+    /**
+     * Callback for when either the device is flashed or errors.
+     * Maybe should re-think what this does.
+     */
     Callback<void(const BootloaderCompletionState)> cbComplete;
 
     /**
      * Maps an index (of type BootloaderCommandIndex) to a command ID.
+     * This basically only exits for additional bootloader versions and
+     * to support several variations of the erase command.
      */
     uint8_t commandMap[DEVICE_BOOTLOADER_COMMANDS_LENGTH];
 
-    //typedef void (*commandHandlerMap) (void);
+    /**
+     * Maps a command index to a function pointer:
+     * commandHandlers[Get] = &someFunction;
+     */
     CommandHandler commandHandlers[DEVICE_BOOTLOADER_COMMANDS_LENGTH];
 
+    /**
+     * Number of times the device has been reset.
+     */
     int resetCount;
 
 
-    void sendCallback(const BootloaderCompletionStatus status, const char* message) {
-        if (this->cbComplete) {
-            BootloaderCompletionState state = {
-                status,
-                message
-            };
-
-            this->cbComplete(state);
-        }
-    }
-    void initializeCommandHandlers() {
-        commandMap[Get] = 0x00; // the value is ALWAYS 0x00 for the Get command
-
-        commandHandlers[Get] = &USARTBootloader::handleGet;
-        commandHandlers[GetVersionAndRPS] = &USARTBootloader::handleGetVersionAndRPS;
-        commandHandlers[GetID] = &USARTBootloader::handleGetID;
-        commandHandlers[ReadMemory] = &USARTBootloader::handleReadMemory;
-        commandHandlers[Go] = &USARTBootloader::handleGo;
-        commandHandlers[WriteMemory] = &USARTBootloader::handleWriteMemory;
-        commandHandlers[EraseMemory] = &USARTBootloader::handleEraseMemory;
-        commandHandlers[WriteProtect] = &USARTBootloader::handleWriteProtect;
-        commandHandlers[ReadoutProtect] = &USARTBootloader::handleReadoutProtect;
-        commandHandlers[ReadoutUnprotect] = &USARTBootloader::handleReadoutUnprotect;
-    }
+    void sendCallback(const BootloaderCompletionStatus status, const char* message);
+    void initializeCommandHandlers();
 
     void handleGet();
     void handleGetVersionAndRPS();
@@ -116,22 +121,26 @@ private:
     void handleWriteUnprotect();
     void handleReadoutProtect();
     void handleReadoutUnprotect();
+    
+    void handleIOEvent();
+    void destroy();
+    void handleReadRequested();
 
 public:
     USARTBootloader(PinName _boot0, PinName _nrst, PinName _tx, PinName _rx)
-        : logger("USARTBootloader"), lastCommandSent(-1), queue(32 * EVENTS_EVENT_SIZE),
-        boot0(_boot0), nrst(_nrst), usart(_tx, _rx, USART_BAUDRATE),
-        resetCount(0)
-          
+        : logger("USARTBootloader"), lastCommandSent(NoLastCommand), queue(32 * EVENTS_EVENT_SIZE),
+          boot0(_boot0), nrst(_nrst), usart(_tx, _rx, DEVICE_USART_BAUDRATE),
+          resetCount(0)
     {
         logger.newline().write("initializing...");
         initializeCommandHandlers();
+
         /*
          * The format is 8 bits, even parity, and stop=1
          * It must be set to blocking, or else it can lead to a weird condition where shit happens during a read/write.
          */
         usart.set_format(8, UARTSerial::Even, 1);
-        usart.set_blocking(true);
+        usart.set_blocking(true);        
         usart.sigio(callback(this, &USARTBootloader::handleIOEvent));
 
         this->eventThread.start(
@@ -140,34 +149,30 @@ public:
 
         this->initializeSlaveDevice();
     }
+
     ~USARTBootloader() {
         this->destroy();
     }
 
-    void destroy() {
-        this->queue.break_dispatch();
-        this->eventThread.terminate();
-        int closeStatus = this->usart.close();
-
-        if (closeStatus) {
-            logger.write("failed to close usart, status=%d", closeStatus);
-        }
-        if (cbComplete) {
-            sendCallback(StatusUpdate, "USARTBootloader instance destroyed");
-        }
-    }
+    /**
+     * TODO
+     */
     bool flashSlaveDevice(const char* filename, Callback<void(enum BootloaderCompletionStatus_t, const char *)> callback) {
-        
         if ((!filename || !strlen(filename))) {
             sendCallback(TargetNotFlashed, "no filename passed");
             return false;
         }
 
+        // TODO
         return true;
     }
 
+    /**
+     * Initialize the slave device for flashing; reboots the device into its bootloader
+     */
     void initializeSlaveDevice() {
         logger.write("set boot0, reset; trying to load into bootloader...");
+
         this->boot0.write(1);
 
         if (this->resetSlave()) {
@@ -180,6 +185,9 @@ public:
         }
     }
 
+    /**
+     * Resets the slave by toggling NRST
+     */ 
     bool resetSlave() {
         nrst.input();
         if (nrst.read() == 0) {
@@ -204,121 +212,92 @@ public:
 
         return true;
     }
-
-    void handleIOEvent() {
-        // DO NOT print from this function. It's within an ISR and it will seriously mess things up.
-        // mbed will allow you to do pc.printf() but do NOT do that.
-        short current_events = this->usart.poll(0);
-        
-        if (current_events & POLLIN) {
-            // https://github.com/ARMmbed/mbed-os/blob/master/drivers/UARTSerial.cpp#L126
-            queue.call(callback(this, &USARTBootloader::handleQueueEvent));
-        }
-    }
-
-    void handleQueueEvent() {
-        size_t size;
-        uint8_t cmd = usart_getc(size);
-
-        if (cmd == 0x00 || cmd == 0xC0 || this->resetCount == 0 || cmd == 0xFF) {
-
-            // usually a parity bit error, but the underlying class doesn't detect it
-            logger.write("invalid packet (%X)", cmd);
-            /*
-                was prev doing if cmd == 0x00 || cmd == 0xc0
-                then just do uart_getc again and check that...
-                need more elegant way to do this
-            */
-        }
-        
-        switch(cmd) {
-            case ACK:
-                logger.write(">> START ACK >>>>>>>>>>>>>>");
-                handleACK();
-                logger.write("<< DONE ACK <<<<<<<<<<<<<<<");
-                break;
-            case NACK:
-                logger.write("NACK. TODO: handle this elegantly");
-                break;
-            default:
-                logger.write("<!> invalid command: %x", cmd);
-                
-                if (!queue.has_next() && (this->usart.poll(0) & 1)) {
-                    logger.write("\t-> there seems to be more in the rxfifo and an empty equeue");
-                    logger.write("\t\t-> will re-enqueue another check");
-                    queue.call(callback(this, &USARTBootloader::handleQueueEvent));
-                }
-                break;
-        }
-    }
-
     
+    /**
+     * Puts a single byte into the txfifo and ensures it sends.
+     */ 
     size_t usart_putc(uint8_t b) {
         size_t written = usart.write(&b, 1);
-        usart.sync();
 
+        usart.sync();
         return written;
     }
 
-    uint8_t usart_getc(size_t &out) {
-        // https://github.com/ARMmbed/mbed-os/blob/master/drivers/UARTSerial.cpp#L259
-        
+
+    /**
+     * Gets a single byte from the rxfifo.
+     */
+    uint8_t usart_getc() {
         uint8_t tmp;
+
+        usart.read(&tmp, 1);
+
+        return tmp;
+    }
+
+    /**
+     * Gets a single byte from the rxfifo.
+     * 
+     * Related: https://github.com/ARMmbed/mbed-os/blob/master/drivers/UARTSerial.cpp#L259
+     */ 
+    uint8_t usart_getc(size_t &out) {
+        uint8_t tmp;
+
         out = usart.read(&tmp, 1);
 
         return tmp;
     }
 
+
+    /**
+     * Transmits a command to the slave device
+     */
     void usart_writeBootloaderCommand(const BootloaderCommandIndex command) {
         // add mutex? should be fine w/o though
-        uint8_t b0 = command == Get ? 0x00 : commandMap[command]; // note: Get is always 0x00
+        uint8_t b0 = (command == Get) ? 0x00 : commandMap[command];
         uint8_t tx[2] = { b0, (uint8_t)(0xFF ^ b0) };
+
         lastCommandSent = b0;
         logger.write("\tWRITE cmd pair <%X, %X>", tx[0], tx[1]);
+
         this->usart.write(tx, 2);
         this->usart.sync();
     }
     
+    /**
+     * Handle an ACK sent by the slave device.
+     */
     void handleACK() {
-        logger.write("\thandling ACK, after cmd(%x) in sequence", lastCommandSent);
+        logger.write("\thandling ACK for %X", lastCommandSent);
 
-        switch(lastCommandSent) {
-            // first ACK
-            case NoLastCommand: {
-                logger.write("\tnext command = get version (0x00)");
+        // if this is the first ACK after a reset
+        if (lastCommandSent == NoLastCommand) {
+            logger.write("\thandshake: OK");
 
-                if (resetCount > 1) {
-                    logger.write("\thalting until I get this figured out");
-                } else {
-                    usart_writeBootloaderCommand(Get);
-                }
+            if (resetCount > 1) {
+                logger.write("\thalting until I get this figured out");
+            } else {
+                usart_writeBootloaderCommand(Get);
             }
-            break;
 
-            default: {
-                uint8_t* commandMapPtr = std::find(
-                    commandMap,
-                    commandMap + DEVICE_BOOTLOADER_COMMANDS_LENGTH,
-                    lastCommandSent
-                );
-
-                if (commandMapPtr == commandMap + DEVICE_BOOTLOADER_COMMANDS_LENGTH) {
-                    logger.write("<!> no handler found for command %x!", lastCommandSent);
-                } else {
-                    int commandIndex = commandMapPtr - commandMap;
-                    CommandHandler handler = commandHandlers[commandIndex];
-                    (this->*handler)();
-                }
-            }
-            break;
-
-
-            case 0x73: {
-                size_t s;
-                logger.write("\twrite protect? acked?=%x\n", this->usart_getc(s));
-            }
-            break;
+            return;
         }
+
+        // basically: find which handler to use
+        uint8_t* commandMapPtr = std::find(
+            commandMap,
+            commandMap + DEVICE_BOOTLOADER_COMMANDS_LENGTH,
+            lastCommandSent
+        );
+
+        if (commandMapPtr == commandMap + DEVICE_BOOTLOADER_COMMANDS_LENGTH) {
+            logger.write("<!> no handler found for command %x!", lastCommandSent);
+        } else {
+            int commandIndex = commandMapPtr - commandMap;
+            CommandHandler handler = commandHandlers[commandIndex];
+            (this->*handler)();
+        }   
+    
     }
 };
 
